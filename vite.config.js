b1497c59371b71f80c82f7cb98e1a19d08d6f9fb
Muhamed Zumair <mohozumair26@gmail.com>
2025-12-28 01:47:08 +0530

    Move properties.json to public folder

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Estate-Agent-Client-side-Web-Application/',
   plugins: [react()],
 })
+
